de4f152e894743afb237cb550ba44fa69c77f3f8
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-07-19 04:52:45 +0200

    first render

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// default camera sensor size in blender - 36mm width
+// default camera focal length in blender - 50mm length
+// default camera render width in blender - 1920 px
+// default camera render height in blender - 1080 px
+#define WIDTH 120
+#define HEIGHT 120
+#define CHANNELS 3
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include "lib/struct/vertex.cpp"
+#include "lib/struct/face.cpp"
+#include "lib/parser/obj_file.cpp"
+#include "lib/camera/ray_casting.cpp"
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "lib/encoder/stb_image_write.h"
+
+#include <chrono>
+using namespace std::chrono;
+
+int main() {
+    unsigned char *pixels = new unsigned char[WIDTH*HEIGHT*CHANNELS];
+    auto start = high_resolution_clock::now();
+    ObjFile *of = new ObjFile("untitled.obj");
+    auto stop1 = high_resolution_clock::now();
+    auto duration1 = duration_cast<microseconds>(stop1 - start);
+    std::cout << "obj reader: " << duration1.count() << " microseconds" << std::endl;
+    RayCasting *rc = new RayCasting(of,pixels);
+    auto stop2 = high_resolution_clock::now();
+    auto duration2 = duration_cast<microseconds>(stop2 - start);
+    std::cout << "ray caster: " << duration2.count() << " microseconds" << std::endl;
+    stbi_write_png("stbpng.png", WIDTH, HEIGHT, CHANNELS, pixels, WIDTH * CHANNELS);
+    return 0;
+}
